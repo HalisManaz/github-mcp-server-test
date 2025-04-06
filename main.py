282e05932c6ba11e6170c85b3f41271f17f8429b
Halis Manaz <halismanaz@hotmail.com>
2025-04-06 15:57:21 +0300

    Add say_goodbye function and call it in main function

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ def say_hello_to(name):
     print(f"Hello, {name}!")
 
 
+def say_goodbye():
+    print("Goodbye, World!")
+
+
 def main():
     say_hello_world()
     say_hello_to("John")
+    say_goodbye()
 
 
 if __name__ == "__main__":
